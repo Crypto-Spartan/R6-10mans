25ba301b67e22c2ec8bada040a10a63412c5ebc6
Crypto-Spartan <accounts@cryptospartan.net>
2020-07-18 03:16:30 -0400

    uncomment print no longer needed

--- get_players_local.py
+++ get_players_local.py
@@ -12,7 +12,7 @@ players = {}
 script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
 rel_path = "data/sheet_raw.csv"
 abs_file_path = os.path.join(script_dir, rel_path)
-print(abs_file_path)
+#print(abs_file_path)
 with open(abs_file_path, "r") as f:
   csv_reader = csv.reader(f)
   players_raw = [x for x in csv_reader]
