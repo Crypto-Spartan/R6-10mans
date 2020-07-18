9bf0a61026372c14306297055fe84497196432d0
Crypto-Spartan <accounts@cryptospartan.net>
2020-07-18 03:54:58 -0400

    webhook half working with error?

--- testing/webhook_test.py
+++ testing/webhook_test.py
@@ -1,14 +1,24 @@
 from discord import Webhook, AsyncWebhookAdapter
 import aiohttp, asyncio
+import os
+
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+rel_file_path = "conf/discord_webhook.txt"
+abs_file_path = os.path.join(os.path.abspath(parent_dir), rel_file_path)
+
+with open(abs_file_path, 'r') as f:
+	discord_webhook_url = f.read()
+	discord_webhook_url = discord_webhook_url.strip()
+
 
 async def foo():
     async with aiohttp.ClientSession() as session:
-        webhook = Webhook.from_url('https://discordapp.com/api/webhooks/728913048890507264/kaDijgGtC9wKfGKY_R2kLvZWgFf9TQyhbjZa-rj6FIUfxqSdyyZk_dY2_fN8fMnZYrjd', adapter=AsyncWebhookAdapter(session))
+        webhook = Webhook.from_url(discord_webhook_url, adapter=AsyncWebhookAdapter(session))
         webhook_text = '''
         This is pretty cool.
         I think webhooks are awesome.
         probably gonna do it this way 
         '''
-        await webhook.send(webhook_text, username='Foo')
+        await webhook.send(webhook_text, username='10-Mans Bot')
 
 asyncio.run(foo())
