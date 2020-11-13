175f036025219f397f96938effcf03ddb2ac8b14
Crypto-Spartan <accounts@cryptospartan.net>
2020-11-13 02:03:05 -0500

    add trueskill.py

---
+++ trueskill.py
@@ -0,0 +1,12 @@
+#from trueskill import (
+#    quality, quality_1vs1, rate, rate_1vs1, Rating, setup, TrueSkill)
+from trueskill import Rating, quality_1vs1, rate_1vs1
+
+#trueskill_env = TrueSkill()
+
+p1 = Rating(24)
+p2 = Rating(27)
+#p3 = trueskill_env.create_rating(20)
+#p4 = trueskill_env.create_rating(22)
+
+#print(trueskill_env.quality([(p1,p2),(p3,p4)]))
